d804f37fbc9c1dddc67ef06f495b10c8ec5d3f63
kimhyonh <pengkim.professional@gmail.com>
2017-11-21 12:34:54 -0500

    remove space in board.csv

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -12,10 +12,6 @@ import javax.swing.*;
 
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
  * @author 1542745
  */
 public class Main {
